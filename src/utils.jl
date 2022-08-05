dd8bb2f3721d7a4de2199610a90c2abfc1c7fa53
Roy <rwang013@uottawa.ca>
2022-08-05 17:45:16 -0400

    added combinevectors()

---
+++ src/utils.jl
@@ -0,0 +1,22 @@
+
+function combinevectors(x::Vector{Vector{T}})::Vector{T} where T
+
+    if isempty(x)
+        return Vector{T}(undef, 0)
+    end
+
+    N = sum(length(x[i]) for i = 1:length(x))
+
+    y = Vector{T}(undef,N)
+
+    st_ind = 0
+    fin_ind = 0
+    for i = 1:length(x)
+        st_ind = fin_ind + 1
+        fin_ind = st_ind + length(x[i]) - 1
+
+        y[st_ind:fin_ind] = x[i]
+    end
+
+    return y
+end
